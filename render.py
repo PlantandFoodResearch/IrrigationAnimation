877db34573c56c71a6c64f0e3d652a6c9453ed17
Common One <Commonone@PFR.CO.NZ>
2015-11-20 10:43:47 +1300

    Moved shape rendering code to shapes.py

--- render.py
+++ render.py
@@ -6,34 +6,8 @@
 import shapefile
 import numpy
 import pygame.draw #TODO: Move to the example code section!
+from shapes import render_shape
 
-def render(shape, surface, transform):
-	""" Draw the given shape onto the given surface, with the given
-		transformation function applied to all of the points.
-		TODO: Fix the reliance on pygame surfaces...
-	"""
-
-	# This is not the shape you are looking for!
-	#TODO: Supporting at least NULL shapes would probably be a good thing,
-	#		although the generated files don't seem to have other shapes.
-	if shape.shapeType != shapefile.POLYGON:
-		raise ValueError("Unknown shape type %s" %shape.shapeType)
-	
-	# We render the polygon!
-	# Polygons are made of different "parts", which are ordered sets of points
-	# that are assumed to join up, so we render them part-by-part.
-	#TODO: Remove pygame dependency, clean up!
-	
-	for num, part in enumerate(shape.parts):
-		start = part
-		try:
-			end = shape.parts[num + 1]
-		except IndexError:
-			end = -1
-		pygame.draw.polygon(surface, (255, 0, 0),
-			[transform(point) for point in shape.points[start:end]], 1)
-
-			
 if __name__ == "__main__":
 	""" Render and display an example GIS file """
 
@@ -54,7 +28,7 @@ if __name__ == "__main__":
 	offset = shapes[0].points[0]
 	transform = lambda p: (p[0]-offset[0]+500, p[1]-offset[1]+700)
 	for shape in shapes:
-		render(shape, screen, transform)
+		render_shape(screen, shape, transform, (255, 0, 0))
 	pygame.display.update()
 	
 	# Wait for a quit event.
---
+++ shapes.py
@@ -0,0 +1,67 @@
+""" Shape handling functions
+
+	Provides functions for rendering and filling shapes, currently using
+	pygame surfaces.
+	
+	Author: Alastair Hughes
+"""
+
+# Import pygame (for drawing and examples)
+import pygame.draw
+# Import 
+import shapefile
+
+def render_shape(surface, shape, transform, colour, width=1):
+	""" Render the given shape onto the given surface.
+		If width == 0, then the shape will be filled.
+		transform is applied to all of the points in the shape.
+	"""
+	
+	# This is not the shape you are looking for!
+	#TODO: Supporting at least NULL shapes would probably be a good thing,
+	#		although the generated files don't seem to have other shapes.
+	if shape.shapeType != shapefile.POLYGON:
+		raise ValueError("Unknown shape type %s" %shape.shapeType)
+	
+	# We render the polygon!
+	# Polygons are made of different "parts", which are ordered sets of points
+	# that are assumed to join up, so we render them part-by-part.
+	#TODO: Remove pygame dependency, clean up!
+	
+	for num, part in enumerate(shape.parts):
+		try:
+			end = shape.parts[num + 1]
+		except IndexError:
+			end = -1
+		pygame.draw.polygon(surface, colour,
+			[transform(point) for point in shape.points[part:end]], width)
+
+if __name__ == "__main__":
+
+	# GIS file/s
+	GIS_FILES = "H:/My Documents/vis/gis/MediumPatches"
+	#TODO: Figure out how to close the files...
+	sf = shapefile.Reader(GIS_FILES)
+
+	# We use pygame for displaying; import and init.
+	import pygame, pygame.event
+	pygame.init()
+	# Run fullscreen.
+	screen = pygame.display.set_mode()
+	
+	# Render!
+	shapes = sf.shapes()
+	#TODO: This is a workaround for offsets...
+	offset = shapes[0].points[0]
+	transform = lambda p: (p[0]-offset[0]+500, p[1]-offset[1]+700)
+	for shape in shapes:
+		render_shape(screen, shape, transform, (255, 0, 0), 1)
+	pygame.display.update()
+	
+	# Wait for a quit event.
+	running = True
+	while running:
+		event = pygame.event.wait()
+		if event.type == pygame.QUIT:
+			running = False
+
