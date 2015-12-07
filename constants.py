de4ae6b662a911a10672c411dd0af8acbc6039e7
Common One <Commonone@PFR.CO.NZ>
2015-12-07 13:33:13 +1300

    Rearranged

--- constants.py
+++ constants.py
@@ -15,6 +15,9 @@ MAX_FRAMES_PER_DAY = 5 # Maximum number of frames per day
 MIN_FRAMES_PER_DAY = 1 # Minimum number of frames per day
 MAX_TEXT_HEIGHT = 60 # Maximum text height
 MIN_TEXT_HEIGHT = 5 # Minimum text height
+SCALE_MARKER_SIZE = 2 # Marker size, in pixels.
+SCALE_TEXT_OFFSET = 5 # Offset of text from the scale, in pixels.
+SCALE_WIDTH = 20 # Width of the scale, in pixels.
 TEXT_COLOUR = (0, 0, 0) # The colour of any text.
 TEXT_AA = False # Whether or not to antialias the text.
 
@@ -24,7 +27,5 @@ PATCH_NUMBER_FIELD = 'PN' # Field name for patch numbers (in the GIS files).
 
 # Temporary constants:
 # TODO: Make these configurable.
-SCALE_DECIMAL_PLACES = 2 # Decimal places to display on the scale.
-SCALE_MARKER_SIZE = 2 # Marker size, in pixels.
-SCALE_TEXT_OFFSET = 5 # Offset of text from the scale, in pixels.
-SCALE_WIDTH = 60 # Width of the scale, in pixels, including text.
+SCALE_DECIMAL_PLACES = 4 # Decimal places to display on the scale.
+
